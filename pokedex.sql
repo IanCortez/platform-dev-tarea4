2b6cbbc1ecf4190a54f2e95ac1d75f267b85cada
NeZx28 <66690631+NeZx28@users.noreply.github.com>
2021-11-23 22:52:25 -0500

    updated sql

--- pokedex.sql
+++ pokedex.sql
@@ -13,34 +13,35 @@ CREATE TABLE pokemon (
 	varchar(255) habilidad NOT NULL,
 	varchar(255) tipo NOT NULL,
 	text url_imagen NOT NULL,
+	datetime modified_date,
 	PRIMARY KEY (id)
 );
 
 
 -- Values
 INSERT INTO pokemon
-VALUES (1, "Bulbasur", 2.04, "Seed", 15.2, "Overgrow", "Grass", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png");
+VALUES (1, "Bulbasur", 2.04, "Seed", 15.2, "Overgrow", "Grass", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png", NOW());
 
 INSERT INTO pokemon
-VALUES (2, "Ivysaur", 3.03, "Seed", 28.7, "Overgrow", "Grass", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png");
+VALUES (2, "Ivysaur", 3.03, "Seed", 28.7, "Overgrow", "Grass", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png", NOW());
 
 INSERT INTO pokemon
-VALUES (3, "Venusaur", 6.07, "Seed", 220.5, "Overgrow", "Grass", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png");
+VALUES (3, "Venusaur", 6.07, "Seed", 220.5, "Overgrow", "Grass", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png", NOW());
 
 INSERT INTO pokemon
-VALUES (4, "Charmander", 2.00, "Lizard", 18.7, "Blaze", "Fire", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png");
+VALUES (4, "Charmander", 2.00, "Lizard", 18.7, "Blaze", "Fire", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png", NOW());
 
 INSERT INTO pokemon
-VALUES (5, "Charmeleon", 3.07, "Flame", 41.9, "Blaze", "Fire", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png");
+VALUES (5, "Charmeleon", 3.07, "Flame", 41.9, "Blaze", "Fire", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png", NOW());
 
 INSERT INTO pokemon
-VALUES (6, "Charizard", 5.07, "Flame", 199.5, "Blaze", "Fire", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png");
+VALUES (6, "Charizard", 5.07, "Flame", 199.5, "Blaze", "Fire", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png", NOW());
 
 INSERT INTO pokemon
-VALUES (7, "Squirtle", 1.08, "Tiny turtle", 19.8, "Torrent", "Water", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png");
+VALUES (7, "Squirtle", 1.08, "Tiny turtle", 19.8, "Torrent", "Water", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png", NOW());
 
 INSERT INTO pokemon
-VALUES (8, "Wartortle", 3.03, "Turtle", 49.6, "Torrent", "Water", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png");
+VALUES (8, "Wartortle", 3.03, "Turtle", 49.6, "Torrent", "Water", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png", NOW());
 
 INSERT INTO pokemon
-VALUES (9, "Blastoise", 5.03, "Shellfish", 188.5, "Torrent", "Water", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png");
+VALUES (9, "Blastoise", 5.03, "Shellfish", 188.5, "Torrent", "Water", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png", NOW());
